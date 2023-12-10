5efac5916f0e515cb084859d71fcf3cf2c77cb97
Sofia Sousa <sofiavazsousa@gmail.com>
2023-12-10 00:01:05 +0000

    Update header component test

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -7,7 +7,7 @@ import '@testing-library/jest-dom';
 
 describe('Header', () => {
   it('renders the header element', () => {
-    render(<Header headerText='test header' />)
+    render(<Header headerText='test header' icon={""} />)
 
     const heading = screen.findByRole('header', {
       name: 'test header',
