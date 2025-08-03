459a4b43e9f0ee0f948ec4f0beb5822ee158184f
sofiavazsousa <sofiavazsousa@gmail.com>
2025-08-03 02:28:00 +0100

    styling: improve stats animation + add stats count up

--- src/app/components/ui/StatsBar.tsx
+++ src/app/components/ui/StatsBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import { keyframes, styled } from "styled-components";
 
 interface StatsBarProps {
@@ -7,11 +8,35 @@ interface StatsBarProps {
 }
 
 const StatsBar: React.FC<StatsBarProps> = ({ label, value }) => {
+  const [displayValue, setDisplayValue] = useState(0);
+
+  useEffect(() => {
+    let start: number | null = null;
+    let frameId: number;
+
+    const duration = 1000;
+
+    const animate = (timestamp: number) => {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      const percent = Math.min(progress / duration, 1);
+      const currentValue = Math.floor(value * percent);
+      setDisplayValue(currentValue);
+
+      if (percent < 1) {
+        frameId = requestAnimationFrame(animate);
+      }
+    };
+
+    frameId = requestAnimationFrame(animate);
+
+    return () => cancelAnimationFrame(frameId);
+  }, [value]);
   return (
     <Container>
       <LabelStyles>{label}</LabelStyles>
       <Bar value={value}>
-        <LabelStyles value={value}>{value}</LabelStyles>
+        <LabelStyles value={value}>{displayValue}</LabelStyles>
       </Bar>
     </Container>
   );
@@ -27,23 +52,24 @@ const Container = styled.div`
 
 const fill = keyframes<{ value: number }>`
   0% {width: 0%}
-  100% {width: 100%}
+  100% {width: ${(props) => props.value}}
 `;
 
 const Bar = styled.div<{ value: number }>`
-  width: ${(props) => props.value < 100 ? props.value + "%" : 100};
-  background-image: repeating-linear-gradient(130deg,  #03a9f4bd 0 7px, #2196f3d1 5px 15px);
+  width: ${(props) => (props.value < 100 ? props.value + "%" : 100)};
+  background-image: repeating-linear-gradient(
+    130deg,
+    #03a9f4bd 0 7px,
+    #2196f3d1 5px 15px
+  );
   border-radius: inherit;
   text-align: center;
-  animation: ${fill} 2s linear;
+  animation: ${fill} 1000ms linear forwards;
 `;
 
 const LabelStyles = styled.span<{ value?: number }>`
   padding: 5px;
   color: "#000";
   font-weight: 900;
-
-  ${({ value }) => value && `
-    color: #fff;
-  `}
+  color: ${({ value }) => (value ? "#fff" : "#000")};
 `;
