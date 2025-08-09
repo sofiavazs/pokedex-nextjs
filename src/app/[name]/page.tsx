1be650d3fe8d93345b5cfb001d5ca5a1bb2b50e5
sofiavazsousa <sofiavazsousa@gmail.com>
2025-08-10 00:04:02 +0100

    chore: fix types

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -3,21 +3,30 @@ import { PuffLoader } from "react-spinners";
 import { getPokemon, getPokemonSpecies } from "../../api/pokemons";
 import DetailsPage from "../components/DetailsPage/DetailsPage";
 
-export default async function Page({ params }: { params: { name: string } }) {
-  const { name } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ name: string }>;
+}) {
+  const { name } = await params;
   const pokemon = await getPokemon(name);
   const pokemonSpecies = await getPokemonSpecies(name);
-  const evolutionChainId = pokemonSpecies.evolution_chain.url.split('evolution-chain/')[1]?.split('/')[0];
+  const evolutionChainId = pokemonSpecies.evolution_chain.url
+    .split("evolution-chain/")[1]
+    ?.split("/")[0];
 
   return (
     <Suspense
       key={name}
-      fallback={<PuffLoader color="#ef526f" size={100} cssOverride={{ position: "absolute", top: "50vh", right: "50vw" }} />}
+      fallback={
+        <PuffLoader
+          color="#ef526f"
+          size={100}
+          cssOverride={{ position: "absolute", top: "50vh", right: "50vw" }}
+        />
+      }
     >
-      <DetailsPage
-        pokemon={pokemon}
-        evolutionChainId={evolutionChainId}
-      />
+      <DetailsPage pokemon={pokemon} evolutionChainId={evolutionChainId} />
     </Suspense>
   );
-};
+}
